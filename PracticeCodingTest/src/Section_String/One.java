e642ac7b05fb4300171c65a81d00c4f403144234
geun0196 <73518321+geun0196@users.noreply.github.com>
2022-02-15 13:42:06 +0900

    Rename package and add 1 problem

--- PracticeCodingTest/src/Section_String/One.java
+++ PracticeCodingTest/src/Section_String/One.java
@@ -1,6 +1,6 @@
 // 한 개의 문자열을 입력받고, 특정 문자를 입력받아 해당 특정문자가 입력받은 문자열에 몇 개 존재하는지 알아내는 프로그램을 작성하세요.
 
-package Section1;
+package Section_String;
 
 import java.util.*;
 import javax.swing.JOptionPane;
---
+++ PracticeCodingTest/src/Section_String/Three.java
@@ -0,0 +1,34 @@
+//한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요. 문장속의 각 단어는 공백으로 구분됩니다.
+// 첫 줄에 가장 긴 단어를 출력한다. 가장 길이가 긴 단어가 여러개일 경우 문장속에서 가장 앞쪽에 위치한 단어를 답으로 한다.
+
+package Section_String;
+
+import java.util.Scanner;
+
+public class Three {
+	
+	public String solution(String str) {		
+		String answer = "";
+		int max = Integer.MIN_VALUE;
+		String[] s = str.split(" ");
+		
+		for (String x: s) {
+			if(x.length() > max) {
+				max = x.length();
+				answer = x;
+			}
+		}
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		
+		Three three = new Three();
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(three.solution(str));
+	}
+
+}
