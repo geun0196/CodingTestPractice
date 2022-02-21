f47a58f2de386b02e71c0763e52d618c211050dc
geun0196 <73518321+geun0196@users.noreply.github.com>
2022-02-21 11:27:33 +0900

    4,5번 주석추가

--- PracticeCodingTest/src/Section_String/Five.java
+++ PracticeCodingTest/src/Section_String/Five.java
@@ -1,3 +1,5 @@
+// 영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고, 특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
+
 package Section_String;
 
 import java.util.*;
@@ -11,17 +13,16 @@ public class Five {
 		char[] x = str.toCharArray();
 		
 		while(lt < rt) {
-			if(Character.isAlphabetic(x[lt]) && Character.isAlphabetic(x[rt])) {
+			if(!Character.isAlphabetic(x[lt])) lt++;
+			else if(!Character.isAlphabetic(x[rt])) rt--;
+			
+			else{
 				char c = x[lt];
 				x[lt] = x[rt];
 				x[rt] = c;
 				lt++;
 				rt--;
 			}
-			else{
-				lt++;
-				rt--;
-			}	
 		}
 		
 		answer = String.valueOf(x);
